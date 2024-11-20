13e8c8dde0e4dabe4477640a7150fb57944e5511
nailakarimova <naila9r2@gmail.com>
2024-11-20 12:45:06 +0400

    models, connection string is added

--- ExamPortal/Program.cs
+++ ExamPortal/Program.cs
@@ -1,8 +1,14 @@
+using ExamPortal.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DbExamPortalContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
